a6861cf74373aafae38c42a29124a9f9ddab0a4e
Juri F <friindel@mail.ru>
2020-07-01 18:35:01 +0100

    update logger

--- app.rb
+++ app.rb
@@ -1,7 +1,20 @@
 class Logger
+  def initialize
+    @f = File.open 'log.txt', 'a'
+  end
+
+  # class method
   def self.say_something
     puts "Hey"
+  end
+
+  # instance method
+  def log_something topic
+    @f.puts topic
   end
 end
 
 Logger.say_something
+
+logger = Logger.new
+logger.log_something 'message'
