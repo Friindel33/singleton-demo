1732174ea0da93390b1925a186c0b86e8de15339
Juri F <friindel@mail.ru>
2020-07-02 12:19:47 +0100

    Update logger self instance

--- app.rb
+++ app.rb
@@ -3,6 +3,16 @@ class Logger
     @f = File.open 'log.txt', 'a'
   end
 
+  @@x = nil
+
+  def self.instance
+    if @@x == nil
+      @@x = Logger.new
+    end
+
+    return @@x
+  end
+
   # class method
   def self.say_something
     puts "Hey"
@@ -15,6 +25,9 @@ class Logger
 end
 
 Logger.say_something
+Logger.instance.log_something 'heyhey'
+Logger.instance.log_something 'heyheyhey'
+
 
 logger = Logger.new
 logger.log_something 'message'
